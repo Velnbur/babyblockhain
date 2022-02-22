7e8e83b2d11ffb86674e3ea705dd86f2c70cfd81
velnbur <kirillbaybula@gmail.com>
2022-02-22 23:47:48 +0200

    Add sign and verification methods with openssl

--- include/commit.h
+++ include/commit.h
@@ -10,6 +10,8 @@
 
 namespace blockchain {
 
+const unsigned int KEY_SIZE = 1024; // in bits
+const unsigned int SIGN_SIZE = 80;
 /*
 ** Class that represents transaction
 ** in blockchain as list of operations, desription, nonce,
@@ -25,20 +27,29 @@ class Commit {
 
   time_t date;
   std::string message;
+  std::string user_id; // public key
+
+  std::string sign;
 
 public:
   /* Operations now belong to Commit object. */
   Commit(const std::vector<Operation *> &operations, int nonce,
-         const std::string &description);
+         const std::string &description, const std::string &pub_key);
 
   ~Commit();
   const std::string &getDesc() const;
   const std::string &getID() const;
+  const std::string &getUserID() const;
+  const std::string &getSign() const;
   std::string toString() const;
+  void doSign(const std::string &priv_key);
+
+  static bool verify(const std::string &sign, const std::string &pub_key,
+                     const std::string &message);
 
 private:
   /* add hash from SHA1 alg as hexdecimal to end of the string @result*/
-  static void hash_to_string(char hash[HASH_SIZE], std::string &result);
+  static void hash_to_string(char *hash, size_t len, std::string &result);
 
   /* convert low 4 bits of byte to hex char */
   static char low_byte_to_char(char c);
--- src/commit.cpp
+++ src/commit.cpp
@@ -1,14 +1,19 @@
 #include "commit.h"
 #include "sha1.h"
+#include <cstddef>
 #include <ctime>
+#include <openssl/bn.h>
+#include <openssl/dsa.h>
+#include <openssl/ossl_typ.h>
 #include <random>
 #include <string>
 
 namespace bc = blockchain;
 
 bc::Commit::Commit(const std::vector<Operation *> &opers, int nonce,
-                   const std::string &desc)
-    : description(desc), operations(opers), nonce(nonce), date(time(0))
+                   const std::string &desc, const std::string &pub_key)
+    : description(desc), operations(opers), nonce(nonce), date(time(0)),
+      user_id(pub_key)
 {
   format_time(date, message);
   message.push_back('\n');
@@ -22,7 +27,7 @@ bc::Commit::Commit(const std::vector<Operation *> &opers, int nonce,
   char *hash =
       SHA1((message + std::to_string(this->nonce)).c_str(), message.size());
 
-  hash_to_string(hash, ID);
+  hash_to_string(hash, HASH_SIZE, ID);
   message = ID + '\n' + message;
   delete[] hash;
 }
@@ -37,15 +42,79 @@ const std::string &bc::Commit::getDesc() const
 {
   return description;
 }
-
 const std::string &bc::Commit::getID() const
 {
   return ID;
 }
 
+const std::string &bc::Commit::getUserID() const
+{
+  return user_id;
+}
+
+const std::string &bc::Commit::getSign() const
+{
+  return sign;
+}
+
 std::string bc::Commit::toString() const
 {
   return message;
+}
+
+#define KEY_SIZE 256
+
+void bc::Commit::doSign(const std::string &priv_key, const std::string &pub_key)
+{
+  DSA *dsa = DSA_new();
+  DSA_generate_parameters_ex(dsa, KEY_SIZE, NULL, 0, NULL, NULL, NULL);
+  BIGNUM *priv = BN_new();
+  BIGNUM *pub = BN_new();
+  BN_hex2bn(&priv, priv_key.c_str());
+  BN_hex2bn(&pub, pub_key.c_str());
+  DSA_set0_key(dsa, pub, priv);
+
+  DSA_SIG *sig =
+      DSA_do_sign((unsigned char *)message.c_str(), message.size(), dsa);
+
+  BIGNUM *R = BN_new(), *S = BN_new();
+  DSA_SIG_get0(sig, (const BIGNUM **)&R, (const BIGNUM **)&S);
+  char *r_hex = BN_bn2hex(R);
+  char *s_hex = BN_bn2hex(S);
+  sign.clear();
+  sign.append(r_hex);
+  sign.append(s_hex);
+
+  delete[] r_hex;
+  delete[] s_hex;
+  DSA_free(dsa);
+  DSA_SIG_free(sig);
+}
+
+bool bc::Commit::verify(const std::string &sign, const std::string &pub_key,
+                        const std::string &message)
+{
+  bool result = false;
+  DSA *dsa = DSA_new();
+
+  DSA_SIG *sig = DSA_SIG_new();
+  BIGNUM *R = BN_new();
+  BIGNUM *S = BN_new();
+  BIGNUM *pub = BN_new();
+
+  BN_hex2bn(&pub, pub_key.c_str());
+  BN_hex2bn(&R, sign.substr(0, SIGN_SIZE / 2 - 1).c_str());
+  BN_hex2bn(&S, sign.substr(SIGN_SIZE / 2, SIGN_SIZE - 1).c_str());
+
+  DSA_SIG_set0(sig, R, S);
+
+  DSA_set0_key(dsa, pub, NULL);
+
+  result = DSA_do_verify((unsigned char *)message.c_str(), (int)message.size(),
+                         sig, dsa);
+  DSA_SIG_free(sig);
+  DSA_free(dsa);
+  return result;
 }
 
 #define TIME_BUFF_SIZE 21
@@ -78,17 +147,12 @@ char bc::Commit::low_byte_to_char(char ch)
 **  [255, 10, 11, 0] -> ['f', 'f', '0', 'a', '0', 'b', '0', '0'];
 **                       |  255  |    10   |    11   |    0    |
 */
-void bc::Commit::hash_to_string(char hash[HASH_SIZE], std::string &result)
+void bc::Commit::hash_to_string(char *hash, size_t len, std::string &result)
 {
-  char buff[HASH_SIZE * 2]; // size twice as big because you need two chars to
-                            // represent one byte as hexdecimal
-  for (int i = 0; i < HASH_SIZE; ++i) {
+  for (size_t i = 0; i < len; ++i) {
     char left_c = (hash[i] >> 4);
     char right_c = hash[i] - (left_c << 4);
-    buff[i * 2] = low_byte_to_char(left_c);
-    buff[i * 2 + 1] = low_byte_to_char(right_c);
+    result.push_back(low_byte_to_char(left_c));
+    result.push_back(low_byte_to_char(right_c));
   }
-
-  // this stuff is faster than returning new object as result
-  result.append(buff);
 }
